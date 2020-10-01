fd013974f3fa9362d77eb9edf18b89607d7765de
Alexander Osadchy <a.i.osadchy@gmail.com>
2020-10-01 08:26:55 +0300

    Remove detail directory

--- include/utl/scope_exit.hpp
+++ include/utl/scope_exit.hpp
@@ -1,9 +1,29 @@
 #ifndef UTL_SCOPE_EXIT_HPP
 #define UTL_SCOPE_EXIT_HPP
 
+#include <utility>
 #include <utl/anonymous_identifier.hpp>
-#include <utl/detail/scope_exit.hpp>
 
 #define SCOPE_EXIT detail::ScopeExit ANONYMOUS_IDENTIFIER = [&]()
+
+
+namespace detail {
+    template <typename F>
+    class ScopeExit {
+    public:
+        ScopeExit(F &&f)
+                : m_function(std::forward<F>(f)) {
+        }
+
+        ~ScopeExit() {
+            m_function();
+        }
+
+    private:
+        F m_function;
+
+    };
+
+} // namespace detail
 
 #endif // UTL_SCOPE_EXIT_HPP
