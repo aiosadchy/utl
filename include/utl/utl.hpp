35bf47a8d5564cbaff2ef2f7286cbc526c295fcd
Alexander Osadchy <a.i.osadchy@gmail.com>
2020-11-01 22:19:52 +0300

    Add single header file containing all of the library

---
+++ include/utl/utl.hpp
@@ -0,0 +1,14 @@
+#ifndef UTL_UTL_HPP
+#define UTL_UTL_HPP
+
+#include "utl/macro.hpp"
+#include "utl/range.hpp"
+#include "utl/repeat.hpp"
+#include "utl/scope_guard.hpp"
+#include "utl/static_block.hpp"
+#include "utl/storage.hpp"
+#include "utl/type_id.hpp"
+#include "utl/type_info.hpp"
+#include "utl/unique_identifier.hpp"
+
+#endif // UTL_UTL_HPP
