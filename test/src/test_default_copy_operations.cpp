9ccfbf902f8312bc3daabcefa674ab68f0db00b2
Alexander Osadchy <a.i.osadchy@gmail.com>
2021-06-10 22:35:49 +0300

    Add test templates and TODOs

---
+++ test/src/test_default_copy_operations.cpp
@@ -0,0 +1,7 @@
+#include <utl/default_copy_operations.hpp>
+
+#include "test.hpp"
+
+// TODO: tests
+
+TEST_MAIN
---
+++ test/src/test_default_move_operations.cpp
@@ -0,0 +1,7 @@
+#include <utl/default_move_operations.hpp>
+
+#include "test.hpp"
+
+// TODO: tests
+
+TEST_MAIN
