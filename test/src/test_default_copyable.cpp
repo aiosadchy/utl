32d3de87cafc86f01f9b76d6cd52b72044f71251
Alexander Osadchy <a.i.osadchy@gmail.com>
2021-09-14 23:26:23 +0300

    Add tests

--- test/src/test_default_copyable.cpp
+++ test/src/test_default_copyable.cpp
@@ -1,7 +1,34 @@
 #include <utl/default_copyable.hpp>
 
+#include <type_traits>
+
 #include "test.hpp"
 
-// TODO: tests
+
+namespace {
+
+class NonCopyable {
+public:
+    NonCopyable(NonCopyable &&) noexcept {}
+
+};
+
+class Copyable {
+public:
+    Copyable(Copyable &&) noexcept {}
+
+    UTL_DEFAULT_COPYABLE(Copyable)
+
+};
+
+}
+
+TEST(default_copyable) {
+    ASSERT(!std::is_copy_constructible_v<NonCopyable>)
+    ASSERT(!std::is_copy_assignable_v<NonCopyable>)
+
+    ASSERT(std::is_copy_constructible_v<Copyable>)
+    ASSERT(std::is_copy_assignable_v<Copyable>)
+}
 
 TEST_MAIN
