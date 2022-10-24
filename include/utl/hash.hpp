27686b5bb4e836371df85b36198817f013ed23ae
Alexander Osadchy <a.i.osadchy@gmail.com>
2022-10-24 07:40:12 +0300

    make Hash class a lot more user-friendly, add hash combine operator

--- include/utl/hash.hpp
+++ include/utl/hash.hpp
@@ -2,36 +2,48 @@
 #define UTL_HASH_HPP
 
 #include <functional>
+#include <type_traits>
+#include <iostream>
+
+#include "utl/default_copyable.hpp"
 
 namespace utl {
 
-class HashCombine {
+class Hash {
 public:
-    template <typename First, typename... Rest>
-    auto operator()(const First &first, const Rest &...rest) {
-        if constexpr (sizeof...(Rest) == 0) {
-            return first;
-        } else {
-            auto tail = (*this)(rest...);
-            // boost::hash_combine
-            return first ^ (tail + First{0x9e3779b9} + (first << First{6}) + (first >> First{2}));
-        }
+    Hash()
+        : m_hash_value{} {
     }
+
+    UTL_DEFAULT_COPYABLE(Hash)
+
+    template <typename T, typename THash = std::hash<T>>
+    explicit Hash(const T &object, THash &&hash_function = THash{})
+        : m_hash_value{hash_function(object)} {
+        std::cout << "hashing " << object << std::endl;
+    }
+
+    [[nodiscard]] operator std::size_t() const {
+        return m_hash_value;
+    }
+
+    [[nodiscard]] Hash operator^(const Hash &another) const {
+        Hash result = *this;
+        result.m_hash_value ^= another.m_hash_value
+                             + std::size_t{0x9e3779b9}
+                             + (result.m_hash_value << std::size_t{6})
+                             + (result.m_hash_value >> std::size_t{2});
+        return result;
+    }
+
+private:
+    std::size_t m_hash_value;
 
 };
 
-
-template <typename T>
-using Hash = std::hash<T>;
-
-
-template <
-    template <typename> typename THash = Hash,
-    typename THashCombine = HashCombine,
-    typename... Args
->
+template <typename... Args>
 auto hash(Args &&...args) {
-    return THashCombine{}(THash<Args>{}(args)...);
+    return (Hash{args} ^ ...);
 }
 
 } // namespace utl
--- test/src/test_hash.cpp
+++ test/src/test_hash.cpp
@@ -2,10 +2,16 @@
 
 #include "test.hpp"
 
+std::size_t add_one(int x) {
+    return static_cast<std::size_t>(x + 1);
+}
+
 TEST(hash) {
     // TODO: tests
     std::cout << utl::hash(1, 2u, 3l) << std::endl;
     std::cout << utl::hash(3l, 2u, 1) << std::endl;
     std::cout << utl::hash(2u, 1) << std::endl;
     std::cout << utl::hash(1, 2u) << std::endl;
+
+    std::cout << utl::Hash(1, add_one) << std::endl;
 }
