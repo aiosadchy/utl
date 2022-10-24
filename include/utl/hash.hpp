b0ea439cc3ba6fad83f73026c5423ed50acc10c5
Alexander Osadchy <a.i.osadchy@gmail.com>
2022-10-24 20:41:56 +0300

    Add operator<< for Hash class, add incremental hash test

--- include/utl/hash.hpp
+++ include/utl/hash.hpp
@@ -3,7 +3,6 @@
 
 #include <functional>
 #include <type_traits>
-#include <iostream>
 
 #include "utl/default_copyable.hpp"
 
@@ -11,29 +10,35 @@ namespace utl {
 
 class Hash {
 public:
-    Hash()
-        : m_hash_value{} {
-    }
-
     UTL_DEFAULT_COPYABLE(Hash)
 
     template <typename T, typename THash = std::hash<T>>
     explicit Hash(const T &object, THash &&hash_function = THash{})
         : m_hash_value{hash_function(object)} {
-        std::cout << "hashing " << object << std::endl;
     }
 
     [[nodiscard]] operator std::size_t() const {
         return m_hash_value;
     }
 
+    Hash &operator^=(const Hash &another) {
+        m_hash_value ^= another.m_hash_value
+            + std::size_t{0x9e3779b9}
+            + (m_hash_value << std::size_t{6})
+            + (m_hash_value >> std::size_t{2});
+        return *this;
+    }
+
     [[nodiscard]] Hash operator^(const Hash &another) const {
         Hash result = *this;
-        result.m_hash_value ^= another.m_hash_value
-                             + std::size_t{0x9e3779b9}
-                             + (result.m_hash_value << std::size_t{6})
-                             + (result.m_hash_value >> std::size_t{2});
+        result ^= another;
         return result;
+    }
+
+    template <typename T>
+    Hash &operator<<(const T &object) {
+        *this ^= Hash{object};
+        return *this;
     }
 
 private:
@@ -42,8 +47,8 @@ private:
 };
 
 template <typename... Args>
-auto hash(Args &&...args) {
-    return (Hash{args} ^ ...);
+std::size_t hash(Args &&...args) {
+    return (... ^ Hash{args});
 }
 
 } // namespace utl
--- test/src/test_hash.cpp
+++ test/src/test_hash.cpp
@@ -6,6 +6,19 @@ std::size_t add_one(int x) {
     return static_cast<std::size_t>(x + 1);
 }
 
+TEST(incremental) {
+    int a = 1;
+    unsigned b = 2;
+    char c = '3';
+    long d = -1;
+
+    utl::Hash incremental_hash{a};
+    incremental_hash << b << c;
+    incremental_hash << d;
+
+    ASSERT(incremental_hash == utl::hash(a, b, c, d))
+}
+
 TEST(hash) {
     // TODO: tests
     std::cout << utl::hash(1, 2u, 3l) << std::endl;
